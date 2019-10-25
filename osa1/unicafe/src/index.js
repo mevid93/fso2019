9efb54e38d055ab537398059a7ec00161da7f62e
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-10-25 18:24:55 +0300

    Osa1: 1.11

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -17,9 +17,7 @@ const Button = ({ clickFunction, text }) => (
 
 const Statistic = ({text, value}) => {
   return (
-    <div>
-      <p>{text} {value}</p>
-    </div>
+    <tr><td>{text}</td><td>{value}</td></tr>
   )
 }
 
@@ -34,14 +32,16 @@ const Statistics = ({ good, neutral, bad }) => {
     )
   }
   return (
-    <div>
-      <Statistic text={'good'} value={good} />
-      <Statistic text={'neutral'} value={neutral} />
-      <Statistic text={'bad'} value={bad} />
-      <Statistic text={'all'} value={good + neutral + bad} />
-      <Statistic text={'average'} value={(good - bad) / (good + neutral + bad)} />
-      <Statistic text={'positive'} value={good / (good + neutral + bad) * 100} />
-    </div>
+    <table>
+      <tbody>
+        <Statistic text={'good'} value={good} />
+        <Statistic text={'neutral'} value={neutral} />
+        <Statistic text={'bad'} value={bad} />
+        <Statistic text={'all'} value={good + neutral + bad} />
+        <Statistic text={'average'} value={(good - bad) / (good + neutral + bad)} />
+        <Statistic text={'positive'} value={(good / (good + neutral + bad) * 100) + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
