b83f2a643359d19b5c7a455ae174438356a9fd13
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-10-27 12:08:25 +0200

    Osa2: 2.5

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,56 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
-const Header = (props) => {
-  return (
-    <div>
-      <h2>{props.course.name}</h2>
-    </div>
-  )
-}
-
-
-const Part = (props) => {
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-}
-
-
-const Content = ({ course }) => {
-
-  const rows = () => course.parts.map(part =>
-    <Part key={part.id} part={part} />
-  )
-
-  return (
-    <div>
-      {rows()}
-    </div>
-  )
-}
-
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((s, p) => s + p.exercises, 0)
-  return (
-    <div>
-      <p><strong>total of {sum} exercises</strong></p>
-    </div>
-  )
-}
-
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 
 const App = () => {
