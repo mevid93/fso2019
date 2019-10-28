03ab61b9162f7504cc2f90a339a990d4c683802e
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-10-28 19:36:01 +0200

    Osa2: 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   // lisätään henkilö
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.filter(person => person.name === personObject.name).length > 0) {
       alert(`${newName} is already added to phonebook`)
@@ -16,6 +18,7 @@ const App = () => {
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   // lisättävän nimen muokkaus
@@ -23,8 +26,13 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  // lisättävän puhelinnumeron muokkaus
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const rows = () => {
-    return persons.map(person => <p key={person.name}>{person.name}</p>)
+    return persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
   }
 
   return (
@@ -33,6 +41,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
