aa32700288b68fd4e10e74b968563c83a79f8648
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-10-28 19:16:16 +0200

    Osa2: 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  // lisätään henkilö
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  // lisättävän nimen muokkaus
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const rows = () => {
+    return persons.map(person => <p key={person.name}>{person.name}</p>)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {rows()}
+    </div>
+  )
+
+}
+
+export default App
