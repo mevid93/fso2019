85024070388b5f39f9ae729fecf46c2a590b2458
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-10-28 19:47:05 +0200

    Osa2: 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   // lisätään henkilö
   const addPerson = (event) => {
@@ -31,13 +37,25 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  // puhelinnumero rivit
   const rows = () => {
-    return persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+    const p = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    return p.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+  }
+
+  // filtterin muokkaus
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={handleFilterChange} value={filter} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
@@ -49,6 +67,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       {rows()}
     </div>
