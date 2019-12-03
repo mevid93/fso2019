53e816c32fa56136c2fe33a109029be5d8f517dd
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-12-03 16:01:34 +0200

    Osa5: 5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -3,6 +3,43 @@ import loginService from './services/login';
 import blogService from './services/blogs';
 import Blog from './components/Blog';
 
+const BlogCreateForm = ({ blogs, setBlogs }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try {
+      const blog = await blogService.create({ title, author, url })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setBlogs(blogs.concat(blog))
+    } catch (exception) {
+      // error
+    }
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          title: <input type="text" value={title} name="Title" onChange={({ target }) => setTitle(target.value)} />
+        </div>
+        <div>
+          author: <input type="text" value={author} name="Author" onChange={({ target }) => setAuthor(target.value)} />
+        </div>
+        <div>
+          url: <input type="text" value={url} name="Url" onChange={({ target }) => setUrl(target.value)} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
 function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -17,9 +54,10 @@ function App() {
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
-    if(loggedUserJSON) {
+    if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -28,6 +66,7 @@ function App() {
     try {
       const user = await loginService.login({ username, password })
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -58,20 +97,12 @@ function App() {
     )
   }
 
-  const blogList = () => {
-    return (
-      <div>
-        <h2>blogs</h2>
-        <p>{user.name} logged in <button onClick={handleLogout}>logout</button>
-        </p>
-        {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
-      </div>
-    )
-  }
-
   return (
     <div>
-      {user === null ? loginForm() : blogList()}
+      {user !== null ? <h2>blogs</h2> : loginForm()}
+      {user !== null && <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>}
+      {user !== null && <BlogCreateForm blogs={blogs} setBlogs={setBlogs} />}
+      {user !== null && blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
   )
 }
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let TOKEN = null
+
+const setToken = (newToken) => {
+  TOKEN = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll}
+const create = (blogObject) => {
+  const config = {
+    headers: { Authorization: TOKEN }
+  }
+  const request = axios.post(baseUrl, blogObject, config)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, setToken }
