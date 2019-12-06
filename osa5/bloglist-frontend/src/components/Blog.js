a068d178505634e5722a0f4f60227cfcc729d69b
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-12-06 12:23:28 +0200

    Osa5: 5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showAllInfo, setShowAllInfo] = useState(false)
+  const showWhenAllInfo = { display: showAllInfo ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle} onClick={() => setShowAllInfo(!showAllInfo)}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenAllInfo}>
+        <a href={blog.url}>{blog.url}</a>
+        <div>{blog.likes} likes <button>like</button></div>
+        <div>added by {blog.user.name}</div>
+      </div>
+    </div >
+  )
+
+}
+
 
 export default Blog
