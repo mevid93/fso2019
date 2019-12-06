8276a6deecd9e093fea6c47d44b58882229e776c
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-12-06 11:57:42 +0200

    Osa5: 5.5

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -3,46 +3,7 @@ import loginService from './services/login';
 import blogService from './services/blogs';
 import Blog from './components/Blog';
 import { ErrorNotification, InfoNotification } from './components/Notification'
-
-const CreateForm = ({ blogs, setBlogs, setErrorMessage, setInfoMessage }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const handleCreate = async (event) => {
-    event.preventDefault()
-    try {
-      const blog = await blogService.create({ title, author, url })
-      setTitle('')
-      setAuthor('')
-      setUrl('')
-      setBlogs(blogs.concat(blog))
-      setInfoMessage(`a new blog ${blog.title} by ${blog.author} added`)
-      setTimeout(() => setInfoMessage(null), 3000)
-    } catch (exception) {
-      setErrorMessage('Failed to create blog')
-      setTimeout(() => setErrorMessage(null), 3000)
-    }
-  }
-
-  return (
-    <div>
-      <h2>create new</h2>
-      <form onSubmit={handleCreate}>
-        <div>
-          title: <input type="text" value={title} name="Title" onChange={({ target }) => setTitle(target.value)} />
-        </div>
-        <div>
-          author: <input type="text" value={author} name="Author" onChange={({ target }) => setAuthor(target.value)} />
-        </div>
-        <div>
-          url: <input type="text" value={url} name="Url" onChange={({ target }) => setUrl(target.value)} />
-        </div>
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+import { LoginForm, CreateForm } from './components/Form'
 
 function App() {
   const [username, setUsername] = useState('')
@@ -51,6 +12,7 @@ function App() {
   const [blogs, setBlogs] = useState([])
   const [errorMessage, setErrorMessage] = useState(null)
   const [infoMessage, setInfoMessage] = useState(null)
+  const [createBlogVisible, setCreateBlogVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(initialBlogs => {
@@ -87,20 +49,12 @@ function App() {
     setUser(null)
   }
 
-  const loginForm = () => {
-    return (
-      <div>
-        <form onSubmit={handleLogin}>
-          <div>
-            username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
-          </div>
-          <div>
-            password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
+  const handleUsernameChange = ({ target }) => {
+    setUsername(target.value)
+  }
+
+  const handlePasswordChange = ({ target }) => {
+    setPassword(target.value)
   }
 
   return (
@@ -108,9 +62,26 @@ function App() {
       {user !== null ? <h2>blogs</h2> : <h2>log in to application</h2>}
       {errorMessage !== null && <ErrorNotification msg={errorMessage} />}
       {infoMessage !== null && <InfoNotification msg={infoMessage} />}
-      {user == null && loginForm()}
+      {user == null &&
+        <LoginForm
+          handleLogin={handleLogin}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePasswordChange}
+          username={username}
+          password={password}
+        />
+      }
       {user !== null && <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>}
-      {user !== null && <CreateForm blogs={blogs} setBlogs={setBlogs} setErrorMessage={setErrorMessage} setInfoMessage={setInfoMessage} />}
+      {user !== null &&
+        <CreateForm
+          blogs={blogs}
+          setBlogs={setBlogs}
+          setErrorMessage={setErrorMessage}
+          setInfoMessage={setInfoMessage}
+          createBlogVisible={createBlogVisible}
+          setCreateBlogVisible={setCreateBlogVisible}
+        />
+      }
       {user !== null && blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
   )
---
+++ osa5/bloglist-frontend/src/components/Form.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import blogService from '../services/blogs'
+
+// form for creating new blog
+const CreateForm = ({ blogs, setBlogs, setErrorMessage, setInfoMessage, createBlogVisible, setCreateBlogVisible }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const hideWhenVisible = { display: createBlogVisible ? 'none' : '' }
+  const showWhenVisible = { display: createBlogVisible ? '' : 'none' }
+
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try {
+      const blog = await blogService.create({ title, author, url })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setBlogs(blogs.concat(blog))
+      setInfoMessage(`a new blog ${blog.title} by ${blog.author} added`)
+      setTimeout(() => setInfoMessage(null), 3000)
+    } catch (exception) {
+      setErrorMessage('Failed to create blog')
+      setTimeout(() => setErrorMessage(null), 3000)
+    }
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setCreateBlogVisible(true)}>new blog</button>
+      </div>
+      <div style={showWhenVisible}>
+        <h2>create new</h2>
+        <form onSubmit={handleCreate}>
+          <div>
+            title: <input type="text" value={title} name="Title" onChange={({ target }) => setTitle(target.value)} />
+          </div>
+          <div>
+            author: <input type="text" value={author} name="Author" onChange={({ target }) => setAuthor(target.value)} />
+          </div>
+          <div>
+            url: <input type="text" value={url} name="Url" onChange={({ target }) => setUrl(target.value)} />
+          </div>
+          <button type="submit">create</button>
+        </form>
+        <button onClick={() => setCreateBlogVisible(false)}>cancel</button>
+      </div>
+    </div>
+  )
+}
+
+// Form for log in
+const LoginForm = ({ handleLogin, handleUsernameChange, handlePasswordChange, username, password }) => {
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          username <input type="text" value={username} name="Username" onChange={handleUsernameChange} />
+        </div>
+        <div>
+          password <input type="password" value={password} name="Password" onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+export { CreateForm, LoginForm }
