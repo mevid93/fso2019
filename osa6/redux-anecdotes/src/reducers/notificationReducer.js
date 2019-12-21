174471d61d4bf5af739deb6f6e3cbc3ed5eef55a
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-12-21 11:43:41 +0200

    Osa6: 6.9

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,32 @@
+export const setNotification = (content) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    data: {
+      content
+    }
+  }
+}
+
+export const clearNotification = () => {
+  return {
+    type: 'CLEAR_NOTIFICATION',
+    data: {
+      content: ''
+    }
+  }
+}
+
+const initialState = ''
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.data.content
+    case 'CLEAR_NOTIFICATION':
+      return initialState
+    default:
+      return state
+  }
+}
+
+export default reducer
