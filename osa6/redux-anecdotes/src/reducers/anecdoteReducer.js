33a530cc1b1aa99a6d5ebf9c5c826b24cc6eeb1b
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-12-22 11:13:38 +0200

    Osa6: 6.18

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,4 @@
+import anecdoteService from '../services/anecdotes'
 
 export const voteAnecdote = (id) => {
   return {
@@ -13,10 +14,13 @@ export const createAnecdote = (data) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTE',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTE',
+      data: anecdotes
+    })
   }
 }
 
