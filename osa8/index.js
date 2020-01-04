4605dada0e847c7a1ef509c4be13b8e86f635fca
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2020-01-04 12:51:03 +0200

    Osa8: 8.2

--- osa8/index.js
+++ osa8/index.js
@@ -85,17 +85,26 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  }
+
   type Query {
     hello: String!
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
