f6cd69b7644d4c0650caa467ebb1144a1f388244
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2020-01-05 14:10:43 +0200

    Osa8: 8.16

--- osa8/backend/index.js
+++ osa8/backend/index.js
@@ -1,13 +1,16 @@
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, AuthenticationError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
 const Author = require('./models/author')
 const Book = require('./models/book')
 const config = require('./utils/config')
+const jwt = require('jsonwebtoken')
+const User = require('./models/user')
 
 mongoose.set('useFindAndModify', false)
 
 // database address loaded from enviromental variable
 const MONGODB_URI = config.MONGODB_URI
+const JWT_SECRET = config.SECRET_KEY
 
 console.log('connecting to', MONGODB_URI)
 
@@ -21,6 +24,16 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 
 const typeDefs = gql`
+
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
 
   type Author {
     name: String!
@@ -38,6 +51,7 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     hello: String!
     bookCount: Int!
     authorCount: Int!
@@ -46,21 +60,37 @@ const typeDefs = gql`
   }
 
   type Mutation {
+
     addBook(
       title: String!
       published: Int!
       author: String!
       genres: [String!]!
     ): Book
+
     editAuthor(
       name: String!
       setBornTo: Int!
     ): Author
+
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token
+
   }
 `
 
 const resolvers = {
   Query: {
+    me: (root, args, context) => {
+      return context.currentUser
+    },
     bookCount: () => Book.collection.countDocuments(),
     authorCount: () => Author.collection.countDocuments(),
     allBooks: (root, args) => {
@@ -88,7 +118,11 @@ const resolvers = {
     }
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+
       const book = new Book({ ...args })
       let author = await Author.findOne({ name: args.author })
       try {
@@ -104,7 +138,10 @@ const resolvers = {
         })
       }
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
       let author = await Author.findOne({ name: args.name })
       try {
         author.born = args.setBornTo
@@ -114,13 +151,45 @@ const resolvers = {
           invalidArgs: args,
         })
       }
-    }
+    },
+    createUser: (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+
+      return user.save()
+        .catch(error => {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') { // hard coded password
+        throw new UserInputError("wrong credentials")
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    },
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
--- osa8/backend/utils/config.js
+++ osa8/backend/utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-let PORT = process.env.PORT
+let SECRET_KEY = process.env.SECRET_KEY
 let MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  SECRET_KEY
 }
