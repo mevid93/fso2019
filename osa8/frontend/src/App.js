d0e7ef80b4a2101b2581214c7ae2e13efdd9a080
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2020-01-06 14:51:53 +0200

    Osa8: 8.25

--- osa8/frontend/src/App.js
+++ osa8/frontend/src/App.js
@@ -99,9 +99,24 @@ const App = () => {
     }, 10000)
   }
 
+  const updateCacheWith = (addedBook) => {
+    const includedIn = (set, object) => 
+      set.map(p => p.id).includes(object.id)  
+
+    const dataInStore = client.readQuery({ query: ALL_BOOKS })
+    if (!includedIn(dataInStore.allBooks, addedBook)) {
+      client.writeQuery({
+        query: ALL_BOOKS,
+        data: { allBooks : dataInStore.allBooks.concat(addedBook) }
+      })
+    }
+  }
+
   useSubscription(BOOK_ADDED, {
     onSubscriptionData: ({ subscriptionData }) => {
-      window.alert(`Ǹew book (${subscriptionData.data.bookAdded.title}) was added!`)
+      const bookAdded = subscriptionData.data.bookAdded
+      window.alert(`Ǹew book (${bookAdded.title}) was added!`)
+      updateCacheWith(bookAdded)
     }
   })
 
